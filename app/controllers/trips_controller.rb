b92c3294f20deca12a2247cdcf7b786458f2b9ed
Smulligan85 <sean.mulligan85@gmail.com>
2016-06-20 13:22:59 -0700

    Add New Trip form

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,22 @@
+class TripsController < ApplicationController
+  def new
+    @trip = Trip.new
+    @name = params[:name]
+  end
+
+  def create
+    @trip = Trip.create(trip_params)
+    if @trip.save
+      flash[:notice] = "Trip Saved"
+    else
+      flash[:error] = "Trip could not be saved"
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :start_date, :end_date)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   post 'users/results'
 
+  resources :trips
+
   authenticated :user do
     root 'users#show', :as => :authenticated_root
   end
