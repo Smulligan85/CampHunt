afbbe7bbda16eda2190a78421dc3343e12bcfe67
Smulligan85 <sean.mulligan85@gmail.com>
2016-08-21 12:27:38 -0700

    Add ajax form for submitting new supplies

--- app/assets/javascripts/trip.js
+++ app/assets/javascripts/trip.js
@@ -18,9 +18,25 @@ $(function() {
       url: url,
       data: data,
       success: function(response) {
-        $(".table tr:last").after('<tr><td>' + response + '</td></tr>');
+        var respArr = response.split("\n");
+        var supplyName = respArr[0];
+        var supplyQuantity = respArr[1];
+        var tripSupplyId = respArr[2];
+        var authToken = $('input:hidden[name=authenticity_token]').val();
+        $(".table").append(
+          '<tr>' +
+            '<td>' + supplyName + " " + supplyQuantity + " " +
+              '<div style="display:inline-block;">' +
+                '<form class="button_to" method="post" action="/trip_supplies/' + tripSupplyId + '/increase"><input type="hidden" name="_method" value="put"><input type="submit" value="+1"><input type="hidden" name="authenticity_token" value="' + authToken + '"></form>' +
+              '</div>' + " " +
+              '<div style="display:inline-block;">' +
+                '<form class="button_to" method="post" action="/trip_supplies/' + tripSupplyId + '/increase"><input type="hidden" name="_method" value="put"><input type="submit" value="-1"><input type="hidden" name="authenticity_token" value="' + authToken + '"></form>' +
+              '</div>' +
+          '</td>' +
+          '</tr>'
+        );
         $('input#trip_supplies_attributes_0_name').val("");
-        $(".edit_trip")[0].reset();
+        // $(".edit_trip")[0].reset();
       }
     });
     event.preventDefault();
